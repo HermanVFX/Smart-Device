8c949b7d208cf2f643f1022c13c7c6a13021e088
HermanMR <hermanvfx@outlook.com>
2021-08-23 09:22:46 +0300

    Первые правки js

--- build/js/main.js
+++ build/js/main.js
@@ -1,44 +1,44 @@
-"use strict";
+'use strict';
 (function () {
-  var body = document.querySelector(".page-body");
+  var body = document.querySelector('.page-body');
 
-  var footerMenuBtn = document.querySelector(".footer__menu-button");
-  var footerMenuBlock = document.querySelector(".footer__menu");
-  var footerContactBtn = document.querySelector(".footer__contact-button");
-  var footerContactBlock = document.querySelector(".footer__contact-list");
+  var footerMenuBtn = document.querySelector('.footer__menu-button');
+  var footerMenuBlock = document.querySelector('.footer__menu');
+  var footerContactBtn = document.querySelector('.footer__contact-button');
+  var footerContactBlock = document.querySelector('.footer__contact-list');
 
-  var callBtn = document.querySelector(".header__contact-button");
-  var callBlock = document.querySelector(".popup");
-  var callCloseBtn = document.querySelector(".popup__btn-close");
-  var callOverlay = document.querySelector(".popup-overlay");
+  var callBtn = document.querySelector('.header__contact-button');
+  var callBlock = document.querySelector('.popup');
+  var callCloseBtn = document.querySelector('.popup__btn-close');
+  var callOverlay = document.querySelector('.popup-overlay');
 
-  var tel = document.querySelectorAll(".phone-input");
+  var tel = document.querySelectorAll('.phone-input');
 
-  var popupForm = document.getElementById("popup-form");
-  var popupSendBtn = document.getElementById("popup-send-button");
-  var popupPhone = document.getElementById("popup-tel");
-  var popupName = document.getElementById("popup-name");
-  var popupCheck = document.getElementById("popup-check");
-  var popupQuestion = document.getElementById("popup-question");
+  var popupForm = document.getElementById('popup-form');
+  var popupSendBtn = document.getElementById('popup-send-button');
+  var popupPhone = document.getElementById('popup-tel');
+  var popupName = document.getElementById('popup-name');
+  var popupCheck = document.getElementById('popup-check');
+  var popupQuestion = document.getElementById('popup-question');
 
-  var mainForm = document.getElementById("main-form");
-  var mainSendBtn = document.getElementById("main-send-button");
-  var mainPhone = document.getElementById("main-tel");
-  var mainName = document.getElementById("main-name");
-  var mainCheck = document.getElementById("main-check");
-  var mainQuestion = document.getElementById("main-question");
+  var mainForm = document.getElementById('main-form');
+  var mainSendBtn = document.getElementById('main-send-button');
+  var mainPhone = document.getElementById('main-tel');
+  var mainName = document.getElementById('main-name');
+  var mainCheck = document.getElementById('main-check');
+  var mainQuestion = document.getElementById('main-question');
 
-  var success = document.querySelector(".success");
-  var successOverlay = document.querySelector(".success-overlay");
-  var successBtn = document.querySelector(".success__close");
+  var success = document.querySelector('.success');
+  var successOverlay = document.querySelector('.success-overlay');
+  var successBtn = document.querySelector('.success__close');
   // Открыть попап
   function openPopup() {
     if (callBlock && body && popupName) {
-      callBlock.classList.remove("popup--close");
-      body.classList.add("overlay");
+      callBlock.classList.remove('popup--close');
+      body.classList.add('overlay');
       // Поставил задержку на focus из за бага (Срабатывало через раз)
       setTimeout(function () {
-        if (!callBlock.classList.contains("popup--close")) {
+        if (!callBlock.classList.contains('popup--close')) {
           popupName.focus();
         }
       }, 250);
@@ -48,10 +48,10 @@
   // Закрыть попап
   function closePopup() {
     if (callBlock && body && callBtn) {
-      callBlock.classList.add("popup--close");
-      body.classList.remove("overlay");
+      callBlock.classList.add('popup--close');
+      body.classList.remove('overlay');
       setTimeout(function () {
-        if (callBlock.classList.contains("popup--close")) {
+        if (callBlock.classList.contains('popup--close')) {
           callBtn.focus();
         }
       }, 500);
@@ -60,10 +60,10 @@
   // Окно подтверждения
   function openSuccess() {
     if (callBlock && success && successBtn) {
-      if (!callBlock.classList.contains("popup--close")) {
+      if (!callBlock.classList.contains('popup--close')) {
         closePopup();
       }
-      success.classList.remove("success--close");
+      success.classList.remove('success--close');
       setTimeout(function () {
         successBtn.focus();
       }, 500);
@@ -73,7 +73,7 @@
 
   function closeSuccess() {
     if (success && popupForm && mainForm) {
-      success.classList.add("success--close");
+      success.classList.add('success--close');
       mainForm.reset();
       popupForm.reset();
     }
@@ -81,26 +81,26 @@
 
   // Открыть меню
   function closeAccordeon(elemBtn, elemBlock) {
-    elemBtn.classList.add("accordeon-btn-close");
-    elemBlock.classList.add("accordeon-close");
-    elemBlock.classList.remove("accordeon-open");
+    elemBtn.classList.add('accordeon-btn-close');
+    elemBlock.classList.add('accordeon-close');
+    elemBlock.classList.remove('accordeon-open');
   }
   // Закрыть меню
   function openAccordeon(elemBtn, elemBlock) {
-    elemBtn.classList.remove("accordeon-btn-close");
-    elemBlock.classList.remove("accordeon-close");
-    elemBlock.classList.add("accordeon-open");
+    elemBtn.classList.remove('accordeon-btn-close');
+    elemBlock.classList.remove('accordeon-close');
+    elemBlock.classList.add('accordeon-open');
   }
   // Ошибка
   function error(elem) {
-    elem.classList.add("error");
+    elem.classList.add('error');
   }
 
   function clearError() {
-    var errorElements = document.querySelectorAll(".error");
+    var errorElements = document.querySelectorAll('.error');
     if (errorElements) {
       for (var index = 0; index < errorElements.length; index++) {
-        errorElements[index].classList.remove("error");
+        errorElements[index].classList.remove('error');
       }
     }
   }
@@ -113,14 +113,14 @@
     footerContactBlock
   ) {
     // В случае загрузки js закроет один из пунктов
-    if (!footerMenuBlock.classList.contains("accordeon-close")) {
+    if (!footerMenuBlock.classList.contains('accordeon-close')) {
       closeAccordeon(footerMenuBtn, footerMenuBlock);
     }
 
-    footerMenuBtn.addEventListener("click", function () {
-      if (footerMenuBlock.classList.contains("accordeon-close")) {
+    footerMenuBtn.addEventListener('click', function () {
+      if (footerMenuBlock.classList.contains('accordeon-close')) {
         openAccordeon(footerMenuBtn, footerMenuBlock);
-        if (!footerContactBlock.classList.contains("accordeon-close")) {
+        if (!footerContactBlock.classList.contains('accordeon-close')) {
           closeAccordeon(footerContactBtn, footerContactBlock);
         }
       } else {
@@ -128,10 +128,10 @@
       }
     });
 
-    footerContactBtn.addEventListener("click", function () {
-      if (footerContactBlock.classList.contains("accordeon-close")) {
+    footerContactBtn.addEventListener('click', function () {
+      if (footerContactBlock.classList.contains('accordeon-close')) {
         openAccordeon(footerContactBtn, footerContactBlock);
-        if (!footerMenuBlock.classList.contains("accordeon-close")) {
+        if (!footerMenuBlock.classList.contains('accordeon-close')) {
           closeAccordeon(footerMenuBtn, footerMenuBlock);
         }
       } else {
@@ -150,30 +150,30 @@
     popupName &&
     success
   ) {
-    callBtn.addEventListener("click", function () {
+    callBtn.addEventListener('click', function () {
       openPopup();
     });
-    callCloseBtn.addEventListener("click", function () {
+    callCloseBtn.addEventListener('click', function () {
       closePopup();
       clearError();
     });
-    callOverlay.addEventListener("click", function () {
+    callOverlay.addEventListener('click', function () {
       closePopup();
       clearError();
     });
-    successOverlay.addEventListener("click", function () {
+    successOverlay.addEventListener('click', function () {
       closeSuccess();
     });
-    successBtn.addEventListener("click", function () {
+    successBtn.addEventListener('click', function () {
       closeSuccess();
     });
     // Закрытие popup нажатием esc
     window.onkeydown = function (event) {
       if (event.keyCode === 27) {
-        if (!callBlock.classList.contains("popup--close")) {
+        if (!callBlock.classList.contains('popup--close')) {
           closePopup();
         }
-        if (!success.classList.contains("success--close")) {
+        if (!success.classList.contains('success--close')) {
           closeSuccess();
         }
       }
@@ -188,15 +188,15 @@
     popupQuestion &&
     popupCheck
   ) {
-    popupSendBtn.addEventListener("click", function () {
+    popupSendBtn.addEventListener('click', function () {
       if (
         popupForm.checkValidity() === true &&
         popupCheck.checked &&
         popupPhone.value.length === 17
       ) {
-        localStorage.setItem("name", popupName.value);
-        localStorage.setItem("phone", popupPhone.value);
-        localStorage.setItem("question", popupQuestion.value);
+        localStorage.setItem('name', popupName.value);
+        localStorage.setItem('phone', popupPhone.value);
+        localStorage.setItem('question', popupQuestion.value);
         closePopup();
         setTimeout(function () {
           openSuccess();
@@ -220,15 +220,15 @@
     mainQuestion &&
     mainCheck
   ) {
-    mainSendBtn.addEventListener("click", function () {
+    mainSendBtn.addEventListener('click', function () {
       if (
         mainForm.checkValidity() === true &&
         mainCheck.checked &&
         mainPhone.value.length === 17
       ) {
-        localStorage.setItem("name", mainName.value);
-        localStorage.setItem("phone", mainPhone.value);
-        localStorage.setItem("question", mainQuestion.value);
+        localStorage.setItem('name', mainName.value);
+        localStorage.setItem('phone', mainPhone.value);
+        localStorage.setItem('question', mainQuestion.value);
         setTimeout(function () {
           openSuccess();
         }, 250);
@@ -245,7 +245,7 @@
   // Маска для поля телефона
   // https://javascript.ru/forum/dom-window/63870-kak-sdelat-masku-telefona-v-input-c-7-___-bez-jquery.html
   if (popupPhone && mainPhone) {
-    window.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener('DOMContentLoaded', function () {
       function setCursorPosition(pos, elem) {
         elem.focus();
         if (elem.setSelectionRange) {
@@ -253,18 +253,18 @@
         } else if (elem.createTextRange) {
           var range = elem.createTextRange();
           range.collapse(true);
-          range.moveEnd("character", pos);
-          range.moveStart("character", pos);
+          range.moveEnd('character', pos);
+          range.moveStart('character', pos);
           range.select();
         }
       }
       function mask(event) {
         for (var index = 0; index < tel.length; index++) {
           var elem = tel[index];
-          var matrix = "+7 (___) ___ ____";
+          var matrix = '+7 (___) ___ ____';
           var i = 0;
-          var def = matrix.replace(/\D/g, "");
-          var val = elem.value.replace(/\D/g, "");
+          var def = matrix.replace(/\D/g, '');
+          var val = elem.value.replace(/\D/g, '');
           if (def.length >= val.length) {
             val = def;
           }
@@ -272,39 +272,39 @@
             if (/[_\d]/.test(a) && i < val.length) {
               return val.charAt(i++);
             } else if (i >= val.length) {
-              return "";
+              return '';
             } else {
               return a;
             }
           });
-          if (event.type === "blur") {
+          if (event.type === 'blur') {
             if (elem.value.length === 2) {
-              elem.value = "";
+              elem.value = '';
             }
           } else {
             setCursorPosition(elem.value.length, elem);
           }
         }
       }
-      popupPhone.addEventListener("input", mask, false);
-      popupPhone.addEventListener("focus", mask, false);
-      popupPhone.addEventListener("blur", mask, false);
-      mainPhone.addEventListener("input", mask, false);
-      mainPhone.addEventListener("focus", mask, false);
-      mainPhone.addEventListener("blur", mask, false);
+      popupPhone.addEventListener('input', mask, false);
+      popupPhone.addEventListener('focus', mask, false);
+      popupPhone.addEventListener('blur', mask, false);
+      mainPhone.addEventListener('input', mask, false);
+      mainPhone.addEventListener('focus', mask, false);
+      mainPhone.addEventListener('blur', mask, false);
     });
   }
   // Ловушка Tabindex
   function trapFocus(element) {
-    const focusableEls = element.querySelectorAll(`input, textarea, button`);
-    const firstFocusableEl = focusableEls[0];
-    const lastFocusableEl = focusableEls[focusableEls.length - 1];
-    const KEYCODE_TAB = 9;
+    var focusableEls = element.querySelectorAll('input, textarea, button');
+    var firstFocusableEl = focusableEls[0];
+    var lastFocusableEl = focusableEls[focusableEls.length - 1];
+    var KEYCODE_TAB = 9;
 
     firstFocusableEl.focus();
 
-    document.addEventListener(`keydown`, function (e) {
-      const isTabPressed = e.key === `Tab` || e.keyCode === KEYCODE_TAB;
+    document.addEventListener('keydown', function (e) {
+      var isTabPressed = e.key === 'Tab' || e.keyCode === KEYCODE_TAB;
 
       if (!isTabPressed) {
         return;
